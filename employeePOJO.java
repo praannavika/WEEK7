d4ced4293101610acaf412f6ac87a59e71c71d88
praannavika <63498540+praannavika@users.noreply.github.com>
2020-05-31 12:25:15 +0530

    week7 problems

---
+++ employeePOJO.java
@@ -0,0 +1,35 @@
+package com.sapirnt;
+
+public class employeePOJO {
+
+		String id;
+		String name;
+		String city;
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getCity() {
+			return city;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+	
+
+}
